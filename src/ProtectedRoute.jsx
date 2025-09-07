ce0dcc3799a1ef132b8dfcb6eeb36879b2d6aebb
AmirAjam <amir.ajam2210@gmail.com>
2025-09-07 15:33:42 +0330

    Fixed access token

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./context/useAuth";
+import getLocalStorage from "./utils/getLocalStorage";
+import { getUser } from "./api/userAPi";
 
 
 const ProtectedRoute = ({ children }) => {
-  const { isLogin } = useAuth();
+  const [isLogin,setIsLogin] = useState(true)
+  const token = getLocalStorage("token")
+  console.log(token)
+  useEffect(() => {
+    getUser(token)
+      .then(res => setIsLogin(res.data.success))
+  }, [])
 
+  console.log("isLogin => ",isLogin)
   if (!isLogin) {
     return <Navigate to="/login" replace />
   }
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "https://clockin.liara.run",
+  baseURL: "https://time-tracker.liara.run",
   headers: {
     "Content-Type": "application/json"
   }
